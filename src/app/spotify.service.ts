a4168b6e20ea374e8f048bdb1eaa0a5939c8f5d6
Pablo Grant <grantpablo4@gmail.com>
2021-06-02 23:04:01 -0500

    feature: (incomplete) implement token refresh

--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -1,9 +1,21 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+  HttpResponse,
+} from '@angular/common/http';
 import { taggedTemplate } from '@angular/compiler/src/output/output_ast';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { iif, Observable, of, throwError } from 'rxjs';
+import {
+  catchError,
+  map,
+  mergeMap,
+  retryWhen,
+  take,
+  tap,
+} from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Playlist } from './models/query.interface';
 import { ISpotifyCredentials } from './models/token.interface';
@@ -13,7 +25,7 @@ import { ISpotifyCredentials } from './models/token.interface';
 })
 export class SpotifyService {
   getPlaylist(id: string): Observable<any> {
-    return this.httpClient.get(`/api/playlist/${id}`, {})
+    return this.httpClient.get(`/api/playlist/${id}`, {});
   }
   constructor(
     private readonly httpClient: HttpClient,
@@ -23,10 +35,9 @@ export class SpotifyService {
   private httpHeaders: HttpHeaders = new HttpHeaders();
   setHttpHeaders() {
     if (this.credentials.access_token != null) {
-      this.httpHeaders = this.httpHeaders.set(
-        'Authorization',
-        `Bearer ${this.credentials.access_token}`
-      ).set('content-type', 'application/json')
+      this.httpHeaders = this.httpHeaders
+        .set('Authorization', `Bearer ${this.credentials.access_token}`)
+        .set('content-type', 'application/json');
       return;
     }
 
@@ -71,16 +82,38 @@ export class SpotifyService {
           this.credentials = credentials;
         }),
         catchError((err) => {
-           
-          
-          return of(err)
+          return of(err);
         })
       );
   }
 
   getPlaylists(): Observable<any> {
-    return this.httpClient.get<Playlist>('/api/playlists', {
-      
-    }).pipe(tap((response) => console.log(response)));
+    console.log('get playlists');
+    return this.httpClient
+      .get<Playlist>('/api/playlists/made-for-you', {})
+      .pipe(
+        retryWhen(this._refreshToken.bind(this)),
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  public refreshToken(): Observable<any> {
+    return this.httpClient
+      .post('/api/refresh_token', {})
+      .pipe(tap((val) => console.log(val)));
+  }
+
+  public hello(): Observable<any> {
+    return of();
+  }
+  _refreshToken(error: Observable<any>): Observable<any> {
+    return error.pipe(
+      mergeMap((error: HttpErrorResponse) => 
+        iif(() => error.status == 401, this.refreshToken())
+      ),
+      take(1)
+    );
   }
 }
