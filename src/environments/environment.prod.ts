f144933160f5900b212a8d27e49d5c84cb5b5902
Pablo Grant <grantpablo4@gmail.com>
2021-06-16 09:42:01 -0500

    fix: change redirect_uri for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   client_id: "eddbb51052ca4cd6a3a2c8445cd98921",
-  redirect_uri: "http://localhost:4200/redirect_uri"
+  redirect_uri: "http://saveyourdailymixes.com/redirect_uri"
 };
